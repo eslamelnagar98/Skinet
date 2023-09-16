023fa1e95a3cf1cc517bf537276f4b1e12adf810
eslamelnagar98 <eslamelnagar44@gmailcom>
2023-09-16 17:05:35 +0300

    Refactor Code

--- Core/Interfaces/IOrderService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.Interfaces;
-public interface IOrderService
-{
-    Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress);
-    Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
-    Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
-    Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
-}
